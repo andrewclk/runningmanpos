ec2142b7d52fbac0b62248db82329ac606bd9e9f
andrewclk <andrewcheems@gmail.com>
2017-09-06 09:29:22 +0800

    more complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-  	@orders = Order.all
   	@areas = Area.all
+  	if params[:filter]
+      @a = params[:filter].to_date
+      @orders = Order.where(c_date: @a)
+    else
+      @orders = Order.where(c_date: Date.today)
+    end
   end
 end
